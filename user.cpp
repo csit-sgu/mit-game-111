27312e923ea5e302beba6c746d24bb3d12ea2906
AlexeyNikitin124 <127249788+AlexeyNikitin124@users.noreply.github.com>
2023-05-16 12:55:23 +0400

    CheckCollision (#23)
    * Добавлены векторы, проверяющие столкновение объектов
    
    * Добавлена проверка условия коллизии объектов
    
    * Добавлена проверка активности объектов и их колайдеров
    
    * Упрощены условия выхода из функции, исправлены комментарии
    
    * Исправлено определение ветора d, исправлена опечатка в уловии
    
    * Окончательно исправлена опечатка в уловии
    
    * Исправлена опечатка в условии else, убраны проверки активности объектов и их коллайдеров
    
    * Добавлено определение знака координат вектора q, исправлены комментарии
    
    * Исправлено изменение знака координат вектора q
    
    * Исправлены комментарии

--- user.cpp
+++ user.cpp
@@ -1,6 +1,7 @@
 #include "user.hpp"
 #include "internal.hpp"
 #include <raylib.h>
+#include <cmath>
 
 // Задание CheckCollision.
 //
@@ -21,7 +22,28 @@
 // Ваше решение может сильно отличаться.
 //
 Collision CheckCollision(Object &obj1, Object &obj2)
-{
+{	// вектор, вычисляющий разницу между позициями объектов
+	Vector2 d = obj2.position - obj1.position;
+
+	// вектор, вычисляющий разность расстояния объектов и полусуммы
+	// их размерностей
+	Vector2 q = {
+		abs(d.x) - (obj2.collider.width + obj1.collider.width) / 2,
+		abs(d.y) - (obj2.collider.height + obj1.collider.height) / 2
+	};
+
+	// если по каждой координате вектор неположителен,
+	// то объекты сталкиваются, иначе нет
+	if (q.x <= 0 && q.y <= 0) {
+		// учитываем знак координат вектора d
+		// для определения направления столкновения
+		q.x = std::copysign(q.x, d.x);
+		q.y = std::copysign(q.y, d.y);
+		return {true, q};
+	}
+	else {
+		return {false, {0, 0}};
+	}
 }
 
 // Задание SolveCollision.
